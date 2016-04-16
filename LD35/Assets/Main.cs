3eb02d9447a9325aa1df096cf95e2dad249902f0
Bogdan Tanasoiu <bogdan.tana@gmail.com>
2016-04-16 19:59:26 +0300

    basic input and movement

---
+++ LD35/Assets/Main.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void CreateWall(int width, int height)
+    {
+
+    }
+}
---
+++ LD35/Assets/Vampire.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Vampire : MonoBehaviour
+{
+    private bool _isBusy = false;
+    private const float MOVEMENT_SPEED = .05f;
+
+    private void Update()
+    {
+        var inputMoveH = Input.GetAxis("Horizontal");
+        var inputMoveV = Input.GetAxis("Vertical");
+        var inputShapeshift = Input.GetButtonDown("Shapeshift");
+        var inputFeed = Input.GetButtonDown("Feed");
+
+        if (inputMoveH != 0 && !_isBusy)
+        {
+            StartCoroutine(Move(new Vector3(Mathf.Sign(inputMoveH), 0, 0)));
+        }
+        
+        if (inputMoveV != 0 && !_isBusy)
+        {
+            StartCoroutine(Move(new Vector3(0, 0, Mathf.Sign(inputMoveV))));
+        }
+
+        if (inputShapeshift && !_isBusy)
+        {
+
+        }
+
+        if (inputFeed && !_isBusy)
+        {
+
+        }
+    }
+
+    private IEnumerator Move(Vector3 delta)
+    {
+        var destination = transform.position + delta;
+
+        transform.LookAt(destination);
+
+        var isObstacle = Physics.Raycast(new Ray(destination + Vector3.up, Vector3.down));
+
+        if (!isObstacle)
+        {
+            _isBusy = true;
+
+            while (transform.position != destination)
+            {
+                transform.position = Vector3.MoveTowards(
+                    transform.position,
+                    destination,
+                    MOVEMENT_SPEED);
+
+                yield return null;
+            }
+
+            _isBusy = false;
+        }
+    }
+}

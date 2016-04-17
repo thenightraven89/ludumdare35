cdd9e09cda36e4e174431ca895466c3e3b19ea2d
Bogdan Tanasoiu <bogdan.tana@gmail.com>
2016-04-18 01:47:51 +0300

    animations fix

--- LD35/Assets/Scripts/PatrolBehaviour.cs
+++ LD35/Assets/Scripts/PatrolBehaviour.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class PatrolBehaviour : MonoBehaviour
@@ -9,9 +10,17 @@ public class PatrolBehaviour : MonoBehaviour
     [SerializeField]
     private float _speed;
 
-    private Transform _t;
-    private int _targetIndex = 0;
+    [SerializeField]
+    private float _stationaryTime;
 
+    [SerializeField]
+    private Animator _animator;
+
+    private Transform _t;
+    private int _targetIndex;
+
+    private bool _isResting;
+    
     private void Awake()
     {
         _t = transform;
@@ -20,13 +29,13 @@ public class PatrolBehaviour : MonoBehaviour
 
         _t.position = _waypoints[0].position;
         _t.rotation = _waypoints[0].rotation;
-
-        SetNextWaypoint();
+        _targetIndex = 0;
+        _isResting = false;
     }
 
     private void Update()
     {
-        if (_waypoints.Length < 2) return;
+        if (_waypoints.Length < 2 || _isResting) return;
 
         if (_t.position != _waypoints[_targetIndex].position)
         {
@@ -37,22 +46,30 @@ public class PatrolBehaviour : MonoBehaviour
         }
         else
         {
-            SetNextWaypoint();
+            StartCoroutine(SetNextWaypoint());
         }
     }
 
-    private void SetNextWaypoint()
+    private IEnumerator SetNextWaypoint()
     {
+        _isResting = true;
+        StopAnimation();
+
+        yield return new WaitForSeconds(_stationaryTime);
+
         _targetIndex = (_targetIndex + 1) % _waypoints.Length;
         _t.LookAt(_waypoints[_targetIndex]);
+        _isResting = false;
+        StartAnimation();
     }
 
     internal void StopAnimation()
     {
-        var animators = GetComponentsInChildren<Animator>();
-        foreach (var a in animators)
-        {
-            a.Stop();
-        }
+        _animator.SetBool("Walk", false);
+    }
+
+    internal void StartAnimation()
+    {
+        _animator.SetBool("Walk", true);
     }
 }
